a8f5309d279befa2af91b9e6ebd3e7245a4aba4c
camilololo <juan.correa-mon@uniminuto.edu.co>
2023-11-16 21:19:13 -0500

    Actualizacion

--- config/conexion.php
+++ config/conexion.php
@@ -1,13 +1,25 @@
 <?php
+    session_start();
     class Conectar{
         protected $dbh;
         protected function conexion(){
             try{
-                $conectar=this->dbh=new PDO("mysql:local=localhost;dbname=Basedaticos","root","");
+                $conectar=$this->dbh=new PDO("mysql:local=localhost;dbname=Basedaticos","root","");
                 return $conectar;
-            }catch(Exeption $e){
+            }catch(Exception $e){
                 print "Conexión fallida" . $e->getMessage()."<br>";
                 die();
             }
         }
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+
+        /*TODO: Ruta principal del proyecto */
+        public static function ruta(){
+            //QA
+            return "http://localhost/Proyecto/";
+        }
     }
+    
+    
---
+++ views/Modulos/JS.php
@@ -0,0 +1,14 @@
+<script src="../html/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../html/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../html/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../html/dist/js/demo.js"></script>
+
+<!-- jQuery -->
+<script src="../html/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../html/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../html/dist/js/adminlte.min.js"></script>
